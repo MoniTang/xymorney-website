d73dc388d2e9706f6483457ce8dfc2744997bfad
Tang <istangxy@163.com>
2022-07-23 14:10:08 +0800

    model.ts

--- src/model.ts
+++ src/model.ts
@@ -1,10 +1,14 @@
 const localStorageKeyName = 'recordList';
 const model = {
+    clone(data: RecordItem[] | RecordItem) {
+        return JSON.parse(JSON.stringify(data))
+
+    },
     fetch() {
-        return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]');
+        return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
     },
     save(date: RecordItem[]) {
         window.localStorage.setItem(localStorageKeyName, JSON.stringify(date))
     }
 };
-export { model };
+export default model;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,14 +14,14 @@ import Types from "../components/Types.vue";
 import NumberPad from "../components/numberPad.vue";
 import vue from 'vue';
 import {Component, Watch} from 'vue-property-decorator';
-import {model} from '@/model';
-const recordList:RecordItem[]=model.fetch();
+import model from '@/model';
+const recordList=model.fetch();
 
 @Component({components:{Tags, Note, Types, NumberPad}})
    export default  class Money extends vue{
     tags=['衣','食','住','行','玩'];
     record:RecordItem={tags:[],notes:'',type:'-',amount:0};
-    recordList: RecordItem[]=JSON.parse(window.localStorage.getItem('recordList')||'[]');
+    recordList: RecordItem[]=recordList;
     onUpdateTag(value:string[]){
         this.record.tags=value;
         console.log(value);
@@ -35,7 +35,7 @@ const recordList:RecordItem[]=model.fetch();
         console.log(value);
     }
     saveRecord(){
-        const record2:RecordItem=JSON.parse(JSON.stringify(this.record))
+        const record2:RecordItem=model.clone(this.record);
         record2.createdAt=new Date()
         this.recordList.push(record2);
         console.log(this.recordList);
@@ -43,7 +43,7 @@ const recordList:RecordItem[]=model.fetch();
     }
     @Watch('recordList')
         onRecordListChange(){
-            window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
+            model.save(this.recordList)
     }
 }
 </script>
