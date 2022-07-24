3957c4c65d1a1e832247179a86445d4d3fbc27cf
Tang <istangxy@163.com>
2022-07-24 10:12:19 +0800

    完成label

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Money from '@/views/Money.vue'
 import Label from '@/views/Label.vue'
 import Statistics from '@/views/Statistics.vue'
 import Notfound from '@/views/Notfound.vue'
+import EditLabel from '@/views/EditLabel.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,10 @@ const routes: Array<RouteConfig> = [
   {
     path: '/statistics',
     component: Statistics,
+  },
+  {
+    path: '/label/edit',
+    component: EditLabel,
   },
   {
     path: '/*',
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -15,7 +15,7 @@ import NumberPad from "../components/numberPad.vue";
 import vue from 'vue';
 import {Component, Watch} from 'vue-property-decorator';
 import recordListModel from '@/models/recordListModel';
-import tagListModel from '@/models/recordListModel';
+import tagListModel from '@/models/tagListModel';
 const recordList=recordListModel.fetch();
 const tagList=tagListModel.fetch();
 
