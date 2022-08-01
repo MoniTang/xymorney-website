f270ff3a9a4a5987473738625a4cdbe6a8f646eb
Tang <istangxy@163.com>
2022-08-01 22:56:22 +0800

    deploy

---
+++ precache-manifest.a6d0cc8f5211038a22f6a37164089391.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89303721f7ecbe497f4b",
+    "url": "/xymorney-website/css/app.473db8bf.css"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/xymorney-website/img/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/xymorney-website/img/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/xymorney-website/img/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/xymorney-website/img/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/xymorney-website/img/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/xymorney-website/img/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/xymorney-website/img/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/xymorney-website/img/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/xymorney-website/img/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/xymorney-website/img/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/xymorney-website/img/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/xymorney-website/img/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/xymorney-website/img/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/xymorney-website/img/mstile-150x150.png"
+  },
+  {
+    "revision": "4e857233cbd3bb2d2db4f78bed62a52f",
+    "url": "/xymorney-website/img/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "fbc68c89ef46039969c0abc4ce2f799a",
+    "url": "/xymorney-website/index.html"
+  },
+  {
+    "revision": "89303721f7ecbe497f4b",
+    "url": "/xymorney-website/js/app.a3035c20.js"
+  },
+  {
+    "revision": "91e4c2eec46d1b842dbc",
+    "url": "/xymorney-website/js/chunk-vendors.ab16c092.js"
+  },
+  {
+    "revision": "8877052f83446228714445deac7cfe6c",
+    "url": "/xymorney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/xymorney-website/robots.txt"
+  },
+  {
+    "revision": "74729a57c1222744a68af0d921a2a579",
+    "url": "/xymorney-website/xymorney.png"
+  }
+]);
